69485697dd503fca3ad0f9fdfa92ebe0c370bba3
Woochul Hyun <woochul.hyun91@gmail.com>
2019-05-29 10:26:22 +0900

    version 2

---
+++ todo-version2/todo.js
@@ -0,0 +1,172 @@
+// let todos = [
+//   { id: 1, content: 'HTML', completed: true },
+//   { id: 2, content: 'CSS', completed: true },
+//   { id: 3, content: 'Javascript', completed: false }
+// ];
+// const $inputTodo = document.querySelector('.input-todo');
+// const $todos = document.querySelector('.todos');
+// const $customCheckbox = document.querySelector('.custom-checkbox');
+// const $deleteBtn = document.querySelector('.btn');
+
+// function generateId() {
+//   return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+// }
+
+// function render() {
+//   let html = '';
+//   let completedCount = 0;
+//   todos.forEach((item) => {
+//     html += `<li id="${item.id}" class="todo-item">
+//    <input class="custom-checkbox" type="checkbox" id="ck-${item.id}" ${
+//   item.completed ? 'checked' : ''
+// }>
+//    <label for="ck-${item.id}">${item.content}</label>
+//    <i class="remove-todo far fa-times-circle"></i>
+//  </li>\n`;
+//     if (item.completed) completedCount += 1;
+//   });
+//   $todos.innerHTML = html;
+
+//   document.querySelector('.completed-todos').innerHTML = completedCount;
+//   document.querySelector('.active-todos').innerHTML =        todos.length - completedCount;
+// }
+
+// $todos.onchange = function (e) {
+//   const id = +e.target.parentNode.id;
+
+//   todos = todos.map(todo => (todo.id === id
+//             ? Object.assign({}, todo, { completed: !todo.completed })
+//             : todo));
+//   render();
+// };
+
+// $todos.onclick = function (e) {
+//   if (!e.target.classList.contains('remove-todo')) return;
+//   todos = todos.filter(item => item.id !== +e.target.parentNode.id);
+//   render();
+// };
+
+// $inputTodo.onkeyup = function (e) {
+//   if ($inputTodo.value.trim() === '' || e.keyCode !== 13) return;
+
+//   todos = [
+//     { id: generateId(), content: $inputTodo.value, completed: false },
+//     ...todos
+//   ];
+//   $inputTodo.value = '';
+//   render();
+// };
+
+// $customCheckbox.onchange = function (e) {
+//   todos = todos.map(item => Object.assign({}, item, { completed: e.target.checked }));
+//   render();
+// };
+
+// $deleteBtn.onclick = function (e) {
+//   todos = todos.filter(item => !item.completed);
+//   render();
+// };
+
+// render();
+
+let todos = [
+  { id: 1, content: 'HTML', completed: true },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 3, content: 'Javascript', completed: false }
+];
+const $inputTodo = document.querySelector('.input-todo');
+const $todos = document.querySelector('.todos');
+const $customCheckbox = document.querySelector('.custom-checkbox');
+const $deleteBtn = document.querySelector('.btn');
+const $nav = document.querySelector('.nav');
+let navState = 'all';
+
+function generateId() {
+  return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+}
+
+function render() {
+  let newTodos = [];
+  if (navState === 'all') {
+    newTodos = todos;
+  } else if (navState === 'active') {
+    newTodos = todos.filter(item => !item.completed);
+  } else if (navState === 'completed') {
+    newTodos = todos.filter(item => item.completed);
+  }
+
+  let html = '';
+  let completedCount = 0;
+  newTodos.forEach((item) => {
+    html += `<li id="${item.id}" class="todo-item">
+   <input class="custom-checkbox" type="checkbox" id="ck-${item.id}" ${
+  item.completed ? 'checked' : ''
+}>
+   <label for="ck-${item.id}">${item.content}</label>
+   <i class="remove-todo far fa-times-circle"></i>
+ </li>\n`;
+    if (item.completed) completedCount += 1;
+  });
+  $todos.innerHTML = html;
+
+  document.querySelector('.completed-todos').innerHTML = completedCount;
+  document.querySelector('.active-todos').innerHTML =        newTodos.length - completedCount;
+}
+
+$todos.onchange = function (e) {
+  const id = +e.target.parentNode.id;
+
+  todos = todos.map(todo => (todo.id === id
+            ? Object.assign({}, todo, { completed: !todo.completed })
+            : todo));
+  render();
+};
+
+$todos.onclick = function (e) {
+  if (!e.target.classList.contains('remove-todo')) return;
+  todos = todos.filter(item => item.id !== +e.target.parentNode.id);
+  render();
+};
+
+$inputTodo.onkeyup = function (e) {
+  if ($inputTodo.value.trim() === '' || e.keyCode !== 13) return;
+
+  todos = [
+    { id: generateId(), content: $inputTodo.value, completed: false },
+    ...todos
+  ];
+  $inputTodo.value = '';
+  render();
+};
+
+$customCheckbox.onchange = function (e) {
+  todos = todos.map(item => Object.assign({}, item, { completed: e.target.checked }));
+  render();
+};
+
+$deleteBtn.onclick = function () {
+  todos = todos.filter(item => !item.completed);
+  render();
+};
+
+$nav.onclick = function (e) {
+  if (e.target.nodeName !== 'LI') return;
+
+  [...$nav.children].forEach((item) => {
+    item.classList.remove('active');
+  });
+
+  e.target.classList.add('active');
+
+  if (e.target.id === 'all') {
+    navState = 'all';
+    render();
+  } else if (e.target.id === 'active') {
+    navState = 'active';
+    render();
+  } else {
+    navState = 'completed';
+    render();
+  }
+};
+render();
